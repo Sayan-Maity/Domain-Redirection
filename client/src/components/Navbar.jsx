5b5179a26133007a8ed782365f1f0c701367cf73
Sayan Maity <89021746+Sayan-Maity@users.noreply.github.com>
2024-08-25 12:53:59 +0530

    FEAT/ added new logo

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,9 +1,10 @@
 import Login from "../Login"
+import logo from '/logo.png'
 
 const Navbar = ({ userAddress, setUserAddress }) => {
   return (
     <div className="flex w-full items-center justify-between px-6 py-3 bg-[#1C1610] border border-b border-[#322921]">
-      <span className="text-[#A45509] text-2xl font-semibold">DomainShift</span>
+      <img src={logo} alt="" className="h-14 w-16" />
       <Login
         userAddress={userAddress}
         setUserAddress={setUserAddress}
